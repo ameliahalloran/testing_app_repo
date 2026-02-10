15cc1f979cc686e661b62495b4e64c84a1d4fa2d
Amelia <amelia_halloran@student.uml.edu>
2026-02-10 13:44:53 -0500

    color change

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"587c18f873b8ab57330422bce09047420d9c7f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "397840170"
+    serviceWorkerVersion: "4262218055"
   }
 });
